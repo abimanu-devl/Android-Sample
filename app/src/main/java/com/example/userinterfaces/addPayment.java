4f6f8e15a809cd924cd1c7d38ed3278e3aa17538
Abishek <abishekmanusha@gmail.com>
2020-08-15 14:33:22 +0530

    IT19016962

---
+++ app/src/main/java/com/example/userinterfaces/addPayment.java
@@ -0,0 +1,13 @@
+package com.example.userinterfaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addPayment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_payment);
+    }
+}
---
+++ app/src/main/java/com/example/userinterfaces/paymentHistory.java
@@ -0,0 +1,14 @@
+package com.example.userinterfaces;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class paymentHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payment_history);
+    }
+}
